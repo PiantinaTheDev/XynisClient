623b48966efbd3b447463383c458cc4c7bcc32a3
Piantina <158730881+PiantinaTheDev@users.noreply.github.com>
2025-02-18 17:53:05 +0100

    Update Phantom1.java

--- crash-methods/Phantom1.java
+++ crash-methods/Phantom1.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package us.whitedev.crashers.impl;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
